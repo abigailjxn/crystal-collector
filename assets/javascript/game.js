4f90cd2c43a77dc604bbd448759e0d7c6a8e8552
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2018-09-20 20:53:37 -0500

    added crystals hold random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,57 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
 
 
-// declare variables
-var userSum = 0;
-var targetNumber = 0;
-var crystalA = 0;
-var crystalB = 0;
-var crystalC = 0;
-var crystalD = 0;
-var totalWins = 0;
-var totalLosses = 0;
-var winMessage = "";
-var loseMessage = "";
+    // declare variables
+    var userSum = 0;
+    var targetNumber = 0;
+    var crystalA = Math.floor(Math.random() * (12 - 1)) + 1;
+    var crystalB = Math.floor(Math.random() * (12 - 1)) + 1;
+    var crystalC = Math.floor(Math.random() * (12 - 1)) + 1;
+    var crystalD = Math.floor(Math.random() * (12 - 1)) + 1;
+    var totalWins = 0;
+    var totalLosses = 0;
+    var winMessage = "";
+    var loseMessage = "";
 
-// dispaly random number ()
-targetNumber = Math.floor(Math.random() * (120 - 19)) + 19;
-console.log(targetNumber);
-// on click of crystal, update user score
+    // dispaly random number ()
+    targetNumber = Math.floor(Math.random() * (120 - 19)) + 19;
+    console.log(targetNumber);
+    $("#targetnumber").text(targetNumber);
+    // on click of crystal, update user score
     // make crystal clickable
-// generate 4 different scores and attach value to crystal
-// make each crystal hold random number (1-12)
+$(".crystals").click(function (){
 
-// win, lose states
-// user score > random number = lose
-// user score = random number = win
-// user score < random number = keep adding
 
-// add to win or loss counter
+    $("#crystala").click(function () {
+        console.log(crystalA);
+    });
+    $("#crystalb").click(function () {
+        console.log(crystalB);
+    });
+    $("#crystalc").click(function () {
+        console.log(crystalC);
+    });
+    $("#crystald").click(function () {
+        console.log(crystalD);
+    });
+    // userSum = crystalA;
 
-// reset on win or loss
-// reset state: new random number, random crystals, user score = 0
+})
+    // generate 4 different scores and attach value to crystal
+    // make each crystal hold random number (1-12)
+
+    // on click, add crystal value to userSum
+    // userSum = 
+
+    // win, lose states
+    // user score > random number = lose
+    // user score = random number = win
+    // user score < random number = keep adding
+
+    // add to win or loss counter
+
+    // reset on win or loss
+    // reset state: new random number, random crystals, user score = 0
 
 })
