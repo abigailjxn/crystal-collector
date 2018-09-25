4153effc561d98b9d0fe5565b1299bed71429b1a
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2018-09-25 14:13:30 -0500

    working game!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,138 +1,72 @@
 $(document).ready(function () {
 
-
-
     // declare variables
-    // var userSum = "0";
-    var userSumDisplay = 0;
+    var userSum = 0;
     var targetNumber = 0;
-    var crystalA = Math.floor(Math.random() * (12 - 1)) + 1;
-    var crystalB = Math.floor(Math.random() * (12 - 1)) + 1;
-    var crystalC = Math.floor(Math.random() * (12 - 1)) + 1;
-    var crystalD = Math.floor(Math.random() * (12 - 1)) + 1;
     var totalWins = 0;
     var totalLosses = 0;
-    var winMessage = "";
-    var loseMessage = "";
 
-    // dispaly random number ()
-    // function genTargetNumber () {
-    //     targetNumber = Math.floor(Math.random() * (120 - 19)) + 19;
-    // console.log(targetNumber);
-    // $("#targetnumber").text(targetNumber);
-    // }
-    targetNumber = Math.floor(Math.random() * (120 - 19)) + 19;
-    console.log(targetNumber);
-    $("#targetnumber").text(targetNumber);
-    // on click of crystal, update user score
-    // make crystal clickable
+    function genTargetNumber() {
+        targetNumber = Math.floor(Math.random() * (120 - 19)) + 19;
+        console.log(targetNumber);
+        $("#targetnumber").text(targetNumber);
+    }
 
-    $(".crystals").attr("value", function (index, attr) {
-        var randomNumber = Math.floor(Math.random() * (12 - 1)) + 1;
-        console.log(this, randomNumber);
-        return randomNumber;
-        
-    })
+    genTargetNumber();
 
-    function reset (totalWins, totalLosses) {
-        userSum = 0;
+    function genCrystalValue() {
+        $(".crystals").attr("value", function (index, attr) {
+            var randomNumber = Math.floor(Math.random() * (12 - 1)) + 1;
+            console.log(this, randomNumber);
+            return randomNumber;
 
+        })
+    }
+
+    genCrystalValue();
+
+    function userMessage () {
 
     }
 
-   
-    var userSum = 0;
+    function reset(totalWins, totalLosses) {
+        userSum = 0;
+        genTargetNumber();
+        genCrystalValue();
+        $("#totalwins").text(totalWins);
+        $("#totallosses").text(totalLosses);
+        $(".usermessage").text("");
+
+    }
+
+
     $(".crystals").click(function (event) {
         var randomValue = $(this).attr("value");
-       
         var crystalValue = parseInt(randomValue);
-          parseInt(userSum);
         userSum += crystalValue;
-        console.log(userSum);
 
         $(".usersum").text(userSum);
         if (userSum > targetNumber) {
-            $(".usermessage").text("You went over! Try again!");
-            console.log("lose");
+            $(".usermessage").text("You went over! Click the magic number and try again!");
             totalLosses++;
             $("#totallosses").text(totalLosses);
+            $("#targetnumber").click(function (event) {
+                reset();
+                $(".usersum").text(userSum);
+
+            });
         }
         else if (userSum === targetNumber) {
-            $(".usermessage").text("Congrats! You win! *sparkle sparkle* Let's do it again!");
-            console.log("win");
+            $(".usermessage").text("Congrats! You win! *sparkle sparkle* Click the magic number and let's do it again!");
             totalWins++;
             $("#totalwins").text(totalWins);
+            $("#targetnumber").click(function (event) {
+                reset();
+                $(".usersum").text(userSum);
+            });
         }
     });
-    // $("#crystalb").click(function () {
-    //     console.log(crystalB);
-    //     userSum = userSum + crystalB;
-    //     $(".usersum").text(userSum);
-    //     if (userSum > targetNumber) {
-    //         $(".usermessage").text("You went over! Try again!");
-    //         console.log("lose");
-    //         totalLosses = totalLosses + 1;
-    //         $("#totallosses").text(totalLosses);
-    //     }
-    //     else if (userSum === targetNumber) {
-    //         $(".usermessage").text("Congrats! You win! *sparkle sparkle* Let's do it again!");
-    //         console.log("win");
-    //         totalWins = totalWins + 1;
-    //         $("#totalwins").text(totalWins);
-    //     }
-    // });
-    // $("#crystalc").click(function () {
-    //     console.log(crystalC);
-    //     userSum = userSum + crystalC;
-    //     $(".usersum").text(userSum);
-    //     if (userSum > targetNumber) {
-    //         $(".usermessage").text("You went over! Try again!");
-    //         console.log("lose");
-    //         totalLosses = totalLosses + 1;
-    //         $("#totallosses").text(totalLosses);
-    //     }
-    //     else if (userSum === targetNumber) {
-    //         $(".usermessage").text("Congrats! You win! *sparkle sparkle* Let's do it again!");
-    //         console.log("win");
-    //         totalWins = totalWins + 1;
-    //         $("#totalwins").text(totalWins);
-    //     }
-    // });
-    // $("#crystald").click(function () {
-    //     console.log(crystalD);
-    //     userSum = userSum + crystalD;
-    //     $(".usersum").text(userSum);
-    //     if (userSum > targetNumber) {
-    //         $(".usermessage").text("You went over! Try again!");
-    //         console.log("lose");
-    //         totalLosses = totalLosses + 1;
-    //         $("#totallosses").text(totalLosses);
-    //     }
-    //     else if (userSum === targetNumber) {
-    //         $(".usermessage").text("Congrats! You win! *sparkle sparkle* Let's do it again!");
-    //         console.log("win");
-    //         totalWins = totalWins + 1;
-    //         $("#totalwins").text(totalWins);
-    //     }
-    // });
 
-    //   if (userSum > targetNumber) {
-    //       $(".usermessage").text("You went over! Try again!");
-    //   }
-    //   else if (userSum = targetNumber) {
-    //       $(".usermessage").text("Congrats! You win! *sparkle sparkle* Let's do it again!");
-    //   }
-
-    // generate 4 different scores and attach value to crystal
-    // make each crystal hold random number (1-12)
-
-    // on click, add crystal value to userSum
-    // $(".usersum").text(userSum);
-
-    // win, lose states
-    // user score > random number = lose
-    // user score = random number = win
-    // user score < random number = keep adding
 
     // add to win or loss counter
 
